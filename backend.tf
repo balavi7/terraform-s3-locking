47e79818bd58f05264f4957b680fe97f87ab0314
Bala Vignesh <82412177+balavi7@users.noreply.github.com>
2025-02-17 03:56:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-locking"
+    bucket = "yourbucketname"
     region = "ap-south-1"
     key = "statefile/terraform.tfstate"
     encrypt = "true"
